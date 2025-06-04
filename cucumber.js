4cc771cee6fa3d422937438e5888c92fddc00076
Amos Tan <156202208+TanJieHaoAmos@users.noreply.github.com>
2025-06-04 11:20:37 +0800

    Remove need for cross-env

--- cucumber.js
+++ cucumber.js
@@ -3,8 +3,8 @@ export default {
   // Default configuration (options outside 'profiles' apply to all runs unless overridden by a profile or CLI)
   // These options will be used if no profile is specified.
   paths: ["tests/features/*.feature"], // Default paths
-
-  import: [
+  requireModule: ["ts-node/register"], // Use ts-node for TypeScript support
+  require: [
     "tests/steps/*.ts",
     "tests/features/support/hooks.ts"
   ],
@@ -18,10 +18,7 @@ export default {
   profiles: {
     // --- Profile 1: Desktop Chrome Tests ---
     desktop_chrome: {
-      tags: "@test_google_chrome_desktop", // Specific tags for this profile
-      // You can override paths here if needed for this profile only
-      // paths: ["tests/features/desktop/**/*.feature"],
-      // require: ["tests/steps/desktop-chrome-steps.ts"], // Add/override required files
+      tags: "@test_google_chrome_desktop", // Specific tags for this profil
       format: ["summary", "html:cucumber-report-desktop-chrome.html"] // Different reports
     },
   }
